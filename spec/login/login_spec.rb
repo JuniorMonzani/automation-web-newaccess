2928133a7b770cdd70d1df0b0de230cf0a3a072f
JuniorMonzani <junior.monzani@gmail.com>
2021-02-22 08:41:21 -0300

    Adicionado no Login testes de usuário e senha incorreta, além de testes em comboBox da Configuração de sistema

--- spec/login/login_spec.rb
+++ spec/login/login_spec.rb
@@ -1,9 +1,50 @@
 
-describe "Testes de login - MDAcesso (nuvem)" do
-    it "abrir no browser o login do MDAcesso" do
+describe 'Abre no browser o NewAccessII na nuvem e realiza testes de login', :login do
+
+    it 'Abre no browser e valida se está na tela de login' do
         puts "Acessando a página - Sistema de Controle de Acesso"
-            visit "http://laboratorio.mdacesso.com.br/logon.aspx"
-            sleep 5
-            expect(page.title).to eql "Sistema de Controle de Acesso"
-        end
+
+        #abre a página especificada no browser
+        visit "http://laboratorio.mdacesso.com.br/logon.aspx"
+        #aguarda 3 segundos na página
+        sleep 1
+        #verifica se existe o título esperado na página
+        expect(page.title).to eql "Sistema de Controle de Acesso"
     end
+
+    it 'Valida o login com um usuário que não existe' do
+        puts "Valida o login com um usuário que não existe"
+
+        #abre a página especificada no browser
+        visit "http://laboratorio.mdacesso.com.br/logon.aspx"
+        #procura pelo id do campo de login e insere um usuário que não existe
+        fill_in 'txtUsrLogin', with: 'teste123'
+        #procura pelo id do campo de senha e insere a senha
+        fill_in 'txtUserPassLogin', with: 'senha'
+        #clica no botão Entrar
+        click_button 'Submit1'
+
+        sleep 1
+        #Valida se a mensagem apresentada é a mensagem esperada
+        expect(find('#businessError')).to have_content '- Senha ou login não conferem. Por favor, tente novamente.'
+
+    end
+
+    it 'Valida o login informando a senha incorreta de um usuário existente' do
+        puts "Valida o login informando a senha incorreta de um usuário existente"
+
+        #abre a página especificada no browser
+        visit "http://laboratorio.mdacesso.com.br/logon.aspx"
+        #procura pelo id do campo de login e insere o usuário Admin
+        fill_in 'txtUsrLogin', with: 'admin'
+        #procura pelo id do campo de senha e insere a senha incorreta
+        fill_in 'txtUserPassLogin', with: 'senha'
+        #clica no botão Entrar
+        click_button 'Submit1'
+
+        sleep 1
+        #Valida se a mensagem apresentada é a mensagem esperada
+        expect(find('#businessError')).to have_content '- Senha ou login não conferem. Por favor, tente novamente.'
+    end
+
+end
---
+++ spec/systemconfiguration/systemConfiguration_spec.rb
@@ -0,0 +1,25 @@
+
+
+describe "Realiza o login e acessa a página de configuração do sistema", :systemConfiguration do
+    title = 'Acessa a página e seleciona um valor no combobox'
+
+    context 'abrindo' do
+        before(:all) do
+            visit "http://laboratorio.mdacesso.com.br/logon.aspx"
+        end
+
+        it 'insere o login' do
+            fill_in 'txtUsrLogin', with: 'admin'
+            fill_in 'txtUserPassLogin', with: 'madis2020@'
+            click_button 'Submit1'
+            sleep 1
+        
+            visit('http://laboratorio.mdacesso.com.br/SystemConfiguration/SystemConfigurationEdt.aspx')
+            sleep 1
+
+            puts 'Encontrando o elemento "Tipo de coleta" e selecionando um valor'
+            select('Realtime sem coleta', from: 'MainContentMainMaster_MainContent_ddlCollectType')
+            sleep 2
+        end
+    end
+end
