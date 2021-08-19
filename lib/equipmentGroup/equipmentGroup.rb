85df80703d74435f0900626ae182ed7a4519b0b2
Joao Monzani Junior <junior.monzani@gmail.com>
2021-08-19 19:50:40 -0300

    Incluindo arquivos de classe.

--- lib/equipmentGroup/equipmentGroup.rb
+++ lib/equipmentGroup/equipmentGroup.rb
@@ -1,49 +1,49 @@
 class EquipmentGroup
-    #inclui a biblioteca Capybara para conseguir usar seus elementos dentros das classes e métodos
-    include Capybara::DSL
+  # inclui a biblioteca Capybara para conseguir usar seus elementos dentros das classes e métodos
+  include Capybara::DSL
 
-    attr_accessor :number, :description, :originArea, :destinationArea
+  attr_accessor :number, :description, :originArea, :destinationArea
 
-    def visit_Register_Equipment_Group
-        visit 'http://laboratorio.mdacesso.com.br/Groups/EquipmentGroupEdt.aspx?OperationType=create&chkLastTen=False&currentPaginationIndex=1'
+  def visit_Register_Equipment_Group
+    visit 'http://laboratorio.mdacesso.com.br/Groups/EquipmentGroupEdt.aspx?OperationType=create&chkLastTen=False&currentPaginationIndex=1'
+  end
+
+  # preenche os campos do cadastro de grupo de equipamento
+  def fills_In_Equipment_Group(number, description, originArea, destinationArea, vehicleAccess, hasDestinationArea, daylightSaving)
+    fill_in 'MainContentMainMaster_MainContent_txtNumber', with: number
+    fill_in 'MainContentMainMaster_MainContent_txtDescription', with: description
+    fill_in 'MainContentMainMaster_MainContent_txtOriginArea', with: originArea
+    fill_in 'MainContentMainMaster_MainContent_txtDestinationArea', with: destinationArea
+
+    # verifica o valor informado para o checkbox, se for verdadeiro marca o campo, se for falso desmareca
+    if vehicleAccess == true
+      check('chkControlsVehicle')
+    else
+      uncheck('chkControlsVehicle')
     end
 
-    #preenche os campos do cadastro de grupo de equipamento
-    def fills_In_Equipment_Group(number, description, originArea, destinationArea, vehicleAccess, hasDestinationArea, daylightSaving)
-        fill_in 'MainContentMainMaster_MainContent_txtNumber', with: number
-        fill_in 'MainContentMainMaster_MainContent_txtDescription', with: description
-        fill_in 'MainContentMainMaster_MainContent_txtOriginArea', with: originArea
-        fill_in 'MainContentMainMaster_MainContent_txtDestinationArea', with: destinationArea
-
-        #verifica o valor informado para o checkbox, se for verdadeiro marca o campo, se for falso desmareca
-        if vehicleAccess == true
-            check('chkControlsVehicle')
-        else
-            uncheck('chkControlsVehicle')
-        end
-
-        if hasDestinationArea == true
-            check('chkHasDestinationArea')
-        else
-            uncheck('chkHasDestinationArea')
-        end
-
-        if daylightSaving == true
-            check('MainContentMainMaster_MainContent_chkDaylightSaving')
-        else
-            uncheck('MainContentMainMaster_MainContent_chkDaylightSaving')
-        end
+    if hasDestinationArea == true
+      check('chkHasDestinationArea')
+    else
+      uncheck('chkHasDestinationArea')
     end
 
-    #associa ao grupo o equipamento infromado
-    def associates_Equipment(equipment)
-        select("#{equipment}", from: 'MainContentMainMaster_MainContent_lstEquipment_lstLeft')
-        find('#MainContentMainMaster_MainContent_lstEquipment_btnTransferToRight').click
+    if daylightSaving == true
+      check('MainContentMainMaster_MainContent_chkDaylightSaving')
+    else
+      uncheck('MainContentMainMaster_MainContent_chkDaylightSaving')
     end
+  end
 
-    #desaassocia do grupo o equipamento informado
-    def desassociated_Equipment(equipment)
-        select("#{equipment}", from: 'MainContentMainMaster_MainContent_lstEquipment_lstRight')
-        find('#MainContentMainMaster_MainContent_lstEquipment_btnTransferToLeft').click
-    end
+  # associa ao grupo o equipamento infromado
+  def associates_Equipment(equipment)
+    select(equipment.to_s, from: 'MainContentMainMaster_MainContent_lstEquipment_lstLeft')
+    find('#MainContentMainMaster_MainContent_lstEquipment_btnTransferToRight').click
+  end
+
+  # desaassocia do grupo o equipamento informado
+  def desassociated_Equipment(equipment)
+    select(equipment.to_s, from: 'MainContentMainMaster_MainContent_lstEquipment_lstRight')
+    find('#MainContentMainMaster_MainContent_lstEquipment_btnTransferToLeft').click
+  end
 end
