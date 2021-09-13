17f89beaecfeed543777fd4f2b0429c77d097808
LeandroJOliveira <43957257+LeandroJOliveira@users.noreply.github.com>
2021-09-13 10:33:16 -0300

    Criação casos iniciais tela Tipo de Credencial

--- features/credentialType/registerCredentialType.feature
+++ features/credentialType/registerCredentialType.feature
@@ -1,39 +1,36 @@
 @registerCredentialType
-Feature: registerCredentialType
-    Com um usuário que tenha permissão.
-    Que eu consiga Cadastrar, Aterar e Excluir um Tipo de Credencial.
+Feature: Register Credential Type
+    Como um usuário do sistema que tenha permissão para Cadastrar Tipos de Credenciais.
 
-    Background: registerCredentialType
-        Given que estou na tela de cadastro de Tipo de Credencial
+    Background:
+        Given Que o usuário esteja na página de cadastro de Tipo de Credencial.
 
     @verifyLabelsCredentialType
-    Scenario: verifyLabels
-        Given que eu verifico todos os resources (nomes dos campos) apresentados na página
-        When eu comparo com os resources esperados
-        Then não deve existir alteração nos resources apresentados
+    Scenario: verifyLabelsCredentialType
+        Given Que eu verifico e comparo todos os resources dos campos apresentados na página.
 
     @fieldDescriptionRequiredCredentialType
-    Scenario: fieldDescriptionRequired
+    Scenario: fieldDescriptionRequiredCredentialType
         Given eu encontro o campo "Descrição"
         And não preencho o campo "Descrição"
         When eu clico no botão "Salvar"
         Then deve existir uma validação no campo "Descrição" pois o mesmo é obrigatório
 
     @fieldDescriptionMaxSizeCredentialType
-    Scenario: fieldDescriptionMaxSize
+    Scenario: fieldDescriptionMaxSizeCredentialType
         Given eu encontro o campo "Descição"
         And eu informo 55 caracteres alfanuméricos
         When eu clico no botão "Salvar"
         Then o Tipo de Credencial deve ser salvo com 50 caracteres na Descrição
 
     @verifyValuesAuthenticationCredentialType
-    Scenario: verifyValuesAuthentication
+    Scenario: verifyValuesAuthenticationCredentialType
         Given que eu obternha todos os valores do dropbox "Autenticação"
         When eu comparo com os valores esperados
         Then deve conter todos os valores esperados
 
     @saveWithPersonAuthenticationCredentialType
-    Scenario: saveCredentialTypeWithPersonAuthentication
+    Scenario: saveWithPersonAuthenticationCredentialType
         Given que eu esteja na tela de cadastro de um novo Tipo de Credencial
         And insiro uma descrição
         And seleciono o valor "Pessoa" no campo "Autenticação"
@@ -41,7 +38,7 @@ Feature: registerCredentialType
         Then deve salvar o Tipo de Credencial com autenticação em "Pessoa"
 
     @saveWithVisitorAuthenticationCredentialType
-    Scenario: saveCredentialTypeWithVisitorAuthentication
+    Scenario: saveWithVisitorAuthenticationCredentialType
         Given que eu esteja na tela de cadastro de um novo Tipo de Credencial
         And insiro uma descrição
         And seleciono o valor "Visitante" no campo "Autenticação"
@@ -49,7 +46,7 @@ Feature: registerCredentialType
         Then deve salvar o Tipo de Credencial com autenticação em "Visitante"
 
     @saveWithCredentialAuthenticationCredentialType
-    Scenario: saveCredentialTypeWithCredentialAuthentication
+    Scenario: saveWithCredentialAuthenticationCredentialType
         Given que eu esteja na tela de cadastro de um novo Tipo de Credencial
         And insiro uma descrição
         And seleciono o valor "Credencial" no campo "Autenticação"
---
+++ features/step_definitions/credentialType/registerCredentialType_steps.rb
@@ -0,0 +1,68 @@
+require 'login/login'
+require 'credentialType/CredentialType'
+require 'commom/findElements'
+require 'commom/constants'
+
+  credentialType = CredentialType.new
+  findElements = FindElements.new
+  login = Login.new
+  $password = login.receive_Correct_Pass
+
+  @background
+    Given('Que o usuário esteja na página de cadastro de Tipo de Credencial.') do
+      credentialType.visit_Register_Credential_Type
+      login.make_Login('admin', $password.to_s)
+    end
+
+    @verifyLabelsCredentialType
+      Given('Que eu verifico e comparo todos os resources dos campos apresentados na página.') do
+        expect(page).to have_content('Descrição')
+        expect(page).to have_content('Autenticação')
+      end
+
+    # context 'Valida obrigatoriedade nos campos:' do
+    #   it 'Validação de obrigatoriedade no campo "Descrição"', :fieldDescriptionRequiredCredentialType do
+    #     findElements.input_textbox('MainContentMainMaster_MainContent_txtCredentialTypeDescription', '')
+    #     click_button 'Salvar'
+    #     expect(page).to have_selector("input[oldtitle='Informe a Descrição']")
+    #   end
+    # end
+    
+    # context 'Valida os valores contidos no dropbox:' do
+    #   it 'Autenticação', :verifyValuesAuthenticationCredentialType do
+    #     values = findElements.verify_Values_DropBox('#MainContentMainMaster_MainContent_ddlCredentialTypeAuthentication', $CREDENTIAL_TYPE_AUTHENTICATION)
+    #     expect(values).to be_truthy
+    #   end
+    # end
+      
+    # context 'Salva um tipo de credencial com sucesso' do
+    #   it 'Preenche todos os campos necessários e salvar o Tipo de Credencial como "pessoa" com sucesso', :saveCredentialTypeWithPersonAuthentication do
+    #     findElements.input_textbox('MainContentMainMaster_MainContent_txtCredentialTypeDescription', 'Automação Pessoa')
+    #     findElements.select_option('#MainContentMainMaster_MainContent_ddlCredentialTypeAuthentication', 'Pessoa')
+    #     click_button 'Salvar'
+    #     sleep 1
+    #     expect(page).to have_content('Cadastro de Tipo de Credencial')
+    #   end
+    
+    #   it 'Preenche todos os campos necessários e salvar o Tipo de Credencial como "visitante" com sucesso', :saveCredentialTypeWithVisitorAuthentication do
+    #     findElements.input_textbox('MainContentMainMaster_MainContent_txtCredentialTypeDescription', 'Automação Visitante')
+    #     findElements.select_option('#MainContentMainMaster_MainContent_ddlCredentialTypeAuthentication', 'Visitante')
+    #     click_button 'Salvar'
+    #     sleep 1
+    #     expect(page).to have_content('Cadastro de Tipo de Credencial')
+    #   end
+    
+    #   it 'Preenche todos os campos necessários e salvar o Tipo de Credencial como "credencial" com sucesso', :saveCredentialTypeWithCredentialAuthentication do
+    #     findElements.input_textbox('MainContentMainMaster_MainContent_txtCredentialTypeDescription', 'Automação Credencial')
+    #     findElements.select_option('#MainContentMainMaster_MainContent_ddlCredentialTypeAuthentication', 'Credencial')
+    #     click_button 'Salvar'
+    #     sleep 1
+    #     expect(page).to have_content('Cadastro de Tipo de Credencial')
+    #   end    
+    # end
+    
+    # it 'Validação de quantidade máxima de caracteres no campo "Descrição"', :fieldDescriptionMaxSizeCredentialType do
+    #   findElements.input_textbox('MainContentMainMaster_MainContent_txtCredentialTypeDescription', 'Automação'.ljust(55, 'X'))
+    #   click_button 'Salvar'
+    # end
+  #  end
